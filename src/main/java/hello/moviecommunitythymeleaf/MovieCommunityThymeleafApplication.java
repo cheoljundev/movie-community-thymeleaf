8582bb80e92f8277c1da0c54aeacfaffed1bb4f4
cheoljundev <cheoljundev@gmail.com>
2024-06-13 13:42:31 +0900

    first commit

---
+++ src/main/java/hello/moviecommunitythymeleaf/MovieCommunityThymeleafApplication.java
@@ -0,0 +1,13 @@
+package hello.moviecommunitythymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCommunityThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCommunityThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/moviecommunitythymeleaf/MovieCommunityThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.moviecommunitythymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCommunityThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
