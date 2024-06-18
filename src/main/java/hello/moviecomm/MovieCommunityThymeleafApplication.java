904a108406099897a8e03510772e44aaa98ca85e
cheoljundev <cheoljundev@gmail.com>
2024-06-18 20:26:28 +0900

    불필요한 mapper scan 삭제

--- src/main/java/hello/moviecomm/MovieCommunityThymeleafApplication.java
+++ src/main/java/hello/moviecomm/MovieCommunityThymeleafApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
-@MapperScan(annotationClass = Mapper.class)
 public class MovieCommunityThymeleafApplication {
 
     public static void main(String[] args) {
