770898e6eda659c24aa5d7689cf3523d52870794
cheoljundev <cheoljundev@gmail.com>
2024-06-19 18:39:55 +0900

    현재 접속한 메뉴에 따라 클래스 변경

--- src/main/java/hello/moviecomm/config/GlobalControllerAdvice.java
+++ src/main/java/hello/moviecomm/config/GlobalControllerAdvice.java
@@ -2,10 +2,13 @@ package hello.moviecomm.config;
 
 import hello.moviecomm.board.domain.Board;
 import hello.moviecomm.board.service.BoardService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.List;
 
@@ -17,5 +20,11 @@ public class GlobalControllerAdvice {
     public void globalAttribute(Model model) {
         List<Board> allBoard = boardService.findAllBoard();
         model.addAttribute("allBoard", allBoard);
+    }
+
+    @ModelAttribute("uri")
+    public String currentUri() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        return request.getRequestURI();
     }
 }
