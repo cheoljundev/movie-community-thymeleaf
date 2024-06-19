90b7a06ee095a266f1cd935e0f6fc126cc02c77c
cheoljundev <cheoljundev@gmail.com>
2024-06-19 18:24:27 +0900

    디테일 글보기

--- src/main/java/hello/moviecomm/board/controller/BoardController.java
+++ src/main/java/hello/moviecomm/board/controller/BoardController.java
@@ -1,15 +1,20 @@
 package hello.moviecomm.board.controller;
 
 import hello.moviecomm.board.dto.DbListPostDto;
+import hello.moviecomm.board.dto.DbPostDto;
+import hello.moviecomm.board.dto.DetailPostDto;
 import hello.moviecomm.board.dto.ListPostDto;
 import hello.moviecomm.board.service.BoardService;
 import hello.moviecomm.board.service.PostService;
+import hello.moviecomm.member.domain.Member;
+import hello.moviecomm.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,6 +25,7 @@ public class BoardController {
 
     private final BoardService boardService;
     private final PostService postService;
+    private final MemberService memberService;
 
     @GetMapping("/{boardNo}")
     public String list(@PathVariable("boardNo") Integer boardNo, Model model) {
@@ -29,6 +35,25 @@ public class BoardController {
         model.addAttribute("boardName", boardName);
         model.addAttribute("postList", postList);
         return "board/list";
+    }
+
+    @GetMapping("/{boardNo}/detail")
+    public String detail(@PathVariable("boardNo") Integer boardNo, @RequestParam("postNo") Integer postNo, Model model) {
+        DbPostDto post = postService.findByNo(postNo);
+        String boardName = boardService.findBoardNameByNo(boardNo);
+        String memberName = memberService.findByNo(post.getMemberNo()).getName();
+        DetailPostDto detailPost = DetailPostDto.builder()
+                .postNo(post.getPostNo())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .createAt(post.getCreateAt())
+                .fileName(post.getFileName())
+                .memberName(memberName)
+                .build();
+        model.addAttribute("boardName", boardName);
+        model.addAttribute("boardNo", boardNo);
+        model.addAttribute("post", detailPost);
+        return "board/detail";
     }
 
 }
