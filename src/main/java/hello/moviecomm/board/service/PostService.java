e96f3ea960bfce60cd615a20a02ba2c36919ec79
cheoljundev <cheoljundev@gmail.com>
2024-06-19 13:50:13 +0900

    PostService 완료

---
+++ src/main/java/hello/moviecomm/board/service/PostService.java
@@ -0,0 +1,24 @@
+package hello.moviecomm.board.service;
+
+import hello.moviecomm.board.dto.DbListPostDto;
+import hello.moviecomm.board.dto.DbPostDto;
+import hello.moviecomm.board.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+    public void save(DbPostDto dbPostDto) {
+        postRepository.save(dbPostDto);
+    }
+    public DbPostDto findByNo(Integer postNo) {
+        return postRepository.findByNo(postNo);
+    }
+    List<DbListPostDto> findAll(Integer boardNo) {
+        return postRepository.findAll(boardNo);
+    }
+}
---
+++ src/test/java/hello/moviecomm/board/service/PostServiceTest.java
@@ -0,0 +1,42 @@
+package hello.moviecomm.board.service;
+
+import hello.moviecomm.board.dto.DbPostDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+
+    @Test
+    void save() {
+        DbPostDto post = DbPostDto.builder()
+                .boardNo(1)
+                .title("title")
+                .content("content")
+                .memberNo(1)
+                .build();
+        postService.save(post);
+        Assertions.assertThat(post.getTitle()).isEqualTo("title");
+    }
+
+    @Test
+    void findByNo() {
+        postService.findByNo(1);
+        Assertions.assertThat(postService.findByNo(1).getTitle()).isEqualTo("테스트글");
+    }
+
+    @Test
+    void findAll() {
+        postService.findAll(1);
+        Assertions.assertThat(postService.findAll(1).size()).isEqualTo(1);
+    }
+}
